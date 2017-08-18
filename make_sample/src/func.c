c6d746fefdd814e81ca84f73cd1ced9ca0d1b459
DanBujak <bujak.dan@gmail.com>
2017-08-18 13:54:40 -0400

    Adding existing projects

---
+++ make_sample/src/func.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <main.h>
+
+void printHello(void) {
+
+  printf("Hello makefiles!\n");
+
+  return;
+}
---
+++ make_sample/src/main.c
@@ -0,0 +1,8 @@
+#include <main.h>
+
+int main() {
+  // call a function in another file
+  printHello();
+
+  return(0);
+}
